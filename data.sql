e91e5cfdc4631cf91544da1a4d90fb97c613a1af
tangchen2018 <42495304+tangchen2018@users.noreply.github.com>
2018-10-29 07:23:04 +0800

    Delete data.sql
    upd

--- data.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO `serveradminuser` VALUES ('1', 'admin', 'admin', '123456', null);
-INSERT INTO `sysparam` VALUES ('1', '0', '40', '40', '0', '20', '10000,15000,20000,30000', '2000000', '2000000', '24', '24', null, '09:30-10:00', '20:00-20:30', null, null, '13', '6', null, '1', '1', null, '500', '500', null, '500', '500', null, '6', '3', '7', '4', '40');
-INSERT INTO `user` VALUES ('1', 'admin', 'admin', '202cb962ac59075b964b07152d234b70', '202cb962ac59075b964b07152d234b70', 'admin', '1538477940', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '', '', null, null, null, null);
-
